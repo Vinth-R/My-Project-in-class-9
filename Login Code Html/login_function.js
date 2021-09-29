fff2fe1487204bca05bf897fa8b688345b5fa54a
Vinth-R <84729083+Vinth-R@users.noreply.github.com>
2021-09-29 10:37:02 +0530

    Add files via upload

---
+++ Login Code Html/login_function.js
@@ -0,0 +1,17 @@
+function loginFunction()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="QWERT"&& password=="1234")
+{
+    alert("login succesfully");
+    return false;
+
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
